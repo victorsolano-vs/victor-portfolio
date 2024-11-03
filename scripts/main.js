13acdeece5304ce8d6241b6403952c003b4c7f32
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-11-03 13:04:21 -0400

    initial test of light/dark theme and changed sass color vars to css vars for ease of JS use

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// code to toggle light/dark theme
+const themeToggleBtn = document.getElementById('themeToggle')
+console.log(themeToggleBtn)
+const body = document.body
+
+// check for saved theme or else set to default 
+const savedTheme = localStorage.getItem('theme') || 'dark'
+body.setAttribute('data-theme', savedTheme)
+
+// toggle theme and save to local storage
+themeToggleBtn.addEventListener('click', () => {
+    console.log('hi')
+    const newTheme = body.getAttribute('data-theme') === 'light' ? 'dark' : 'light'
+
+    body.setAttribute('data-theme', newTheme)
+    localStorage.setItem('theme', newTheme)
+})
